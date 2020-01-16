5c9ce4ff5e471788ccc7cff5823f03ddce0858a1
rhaynes3k <rhaynes3k@gmail.com>
2020-01-16 00:54:13 -0600

    added member methods for display

--- members/lib/members/cli.rb
+++ members/lib/members/cli.rb
@@ -4,50 +4,54 @@ class Members::CLI
   def call
     #binding.pry
     Mem_srch.loc_srch
-
      search_query
-    list_members
-    next?
   end
 
-  def list_members
+  def list_members#(state)
     i = 1
-    Member_details.all.each do|m|
+    Member_details.all.select do|m|m.state
       #binding.pry
-
       if m.state.downcase == @st_sel.downcase
         puts "#{i}. #{m.f_name} #{m.l_name} #{m.affl}"
         i+=1
-      # else
-      #   puts "invalid"
       end
-      #binding.pry
     end
-      #binding.pry
   end
 
   def search_query
     puts "Enter your state:"
     @st_sel = gets.chomp
+      sts = []
 
-    #puts "You live in #{state}?"
-    #next?
-  end
+      Member_details.all.each do|s|
+        sts << s.state.downcase
+        #binding.pry
+      end
+        #binding.pry
+      if sts.include?(@st_sel.downcase)
+        list_members#(@st_sel)
+        #binding.pry
+      else
+        puts "No way!"
+        next?
+      end
+    end
 
   def next?
-    puts "Would you like to search again? 1 = Yes 2 = No"
+    puts "Would you like to search again? Yes or No"
     ans = gets.chomp
     case ans
-      when "1"
+    when "Yes"
         search_query
         list_members
         next?
-      when "2"
+      when "No"
         goodbye
     end
   end
 
   def goodbye
     puts "Thank you! Have a great day!"
+    abort
   end
 end
