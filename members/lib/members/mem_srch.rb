e7e2c464d02848666edc5a620566241ab6838e4b
rhaynes3k <rhaynes3k@gmail.com>
2020-01-16 00:55:20 -0600

    iterating over site pages

--- members/lib/members/mem_srch.rb
+++ members/lib/members/mem_srch.rb
@@ -2,7 +2,10 @@ class Mem_srch
 
 
   def self.loc_srch
-    rep = Nokogiri::HTML(open("https://www.congress.gov/members?q={%22congress%22:116}&searchResultViewType=expanded&KWICView=false"))
+
+    page = 1
+    while page < 4
+    rep = Nokogiri::HTML(open("https://www.congress.gov/members?q={%22congress%22:%22116%22}&searchResultViewType=expanded&KWICView=false&pageSize=250&page=#{page}"))
     congs = rep.css(".expanded").each do|cong|
         mem_info = cong.css(".result-heading a").text.split
         f_name = mem_info[2]#rep.css(".expanded")[0].css(".result-heading a").text
@@ -13,7 +16,8 @@ class Mem_srch
         Member_details.new(f_name, l_name, party, state, affl)
         #binding.pry
       end
-
+      page += 1
+    end
   end
 
 end
