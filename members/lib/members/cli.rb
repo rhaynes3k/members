f021f252c6fc566d729fe792669a1a4031a08aa0
rhaynes3k <rhaynes3k@gmail.com>
2020-01-17 02:12:57 -0600

    added more method for level 2 dive

--- members/lib/members/cli.rb
+++ members/lib/members/cli.rb
@@ -1,5 +1,5 @@
 class Members::CLI
-  attr_accessor :st_sel
+  attr_accessor :st_sel, :pick, :party, :g
 
   def call
     #binding.pry
@@ -9,17 +9,22 @@ class Members::CLI
 
   def list_members#(state)
     i = 1
-    Member_details.all.select do|m|m.state
+    @g = Member_details.all.select do|m|m.state
       #binding.pry
       if m.state.downcase == @st_sel.downcase
+        #puts "*****************************************************************"
+
         puts "#{i}. #{m.f_name} #{m.l_name} #{m.affl}"
+
+        puts "*****************************************************************"
+
         i+=1
       end
     end
   end
 
   def search_query
-    puts "Enter your state:"
+    puts "Enter state:"
     @st_sel = gets.chomp
       sts = []
 
@@ -30,6 +35,7 @@ class Members::CLI
         #binding.pry
       if sts.include?(@st_sel.downcase)
         list_members#(@st_sel)
+        more
         #binding.pry
       else
         puts "No way!"
@@ -37,15 +43,30 @@ class Members::CLI
       end
     end
 
+  def more
+    puts "For more information on a congressperson select a number."
+    pick = gets.chomp.to_i
+    x = g.collect{|m|m}
+    mem_pick = x[pick-1].party
+    puts "*****************************************************************"
+    puts "#{x[pick-1].f_name} #{x[pick-1].l_name.delete ","}'s party is #{mem_pick}"
+    puts "#{x[pick-1].f_name}'s served congress's #{x[pick-1].served}"
+    puts "*****************************************************************"
+
+    #mem_pick
+       #binding.pry
+  end
+
   def next?
     puts "Would you like to search again? Yes or No"
     ans = gets.chomp
     case ans
-    when "Yes"
+
+      when "Yes"||"yes"
         search_query
         list_members
         next?
-      when "No"
+      when "No"||"no"
         goodbye
     end
   end
