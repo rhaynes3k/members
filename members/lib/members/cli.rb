1dcc54e0980b789690bd61ddc948e57035f15412
rhaynes3k <rhaynes3k@gmail.com>
2020-01-21 11:19:22 -0600

    added line breaks for appearance, fixed exit options

--- members/lib/members/cli.rb
+++ members/lib/members/cli.rb
@@ -1,42 +1,40 @@
 class Members::CLI
-  attr_accessor :st_sel, :pick, :party, :g
+  attr_accessor :st_sel, :pick, :spec2b, :g
 
   def call
-    #binding.pry
     Mem_srch.loc_srch
      search_query
   end
 
-  def list_members#(state)
+  def list_members
     i = 1
-    @g = Member_details.all.select do|m|m.state
-      #binding.pry
-      if m.state.downcase == @st_sel.downcase
-        #puts "*****************************************************************"
-
+    @g = Member_details.all.select do|m|m.spec1b
+      if m.spec1b.downcase == @st_sel.downcase
         puts "#{i}. #{m.f_name} #{m.l_name} #{m.affl}"
-
         puts "*****************************************************************"
-
         i+=1
       end
     end
   end
 
   def search_query
-    puts "Enter state:"
+    puts "*****************************************************************"
+
+    puts "               Welcome to Congre-search"
+
+    puts "*****************************************************************"
+    puts "    Enter state:"
     @st_sel = gets.chomp
       sts = []
-
       Member_details.all.each do|s|
-        sts << s.state.downcase
-        #binding.pry
+        sts << s.spec1b.downcase
       end
-        #binding.pry
       if sts.include?(@st_sel.downcase)
-        list_members#(@st_sel)
+        puts "The congressional line-up for the state of #{@st_sel.capitalize}"
+        puts
+        list_members
+        puts
         more
-        #binding.pry
       else
         puts "No way!"
         search_query
@@ -44,39 +42,44 @@ class Members::CLI
     end
 
   def more
-    puts "For more information on a congressperson select a number - or select 0 to search again."
+    puts "For more information on a congressperson select a number."
+    puts "Select 0 to search again."
+    puts "Select any key to exit"
     pick = gets.chomp.to_i
     if pick > 0
-      x = g.collect{|m|m}
-    mem_pick = x[pick-1].party
-    puts "*****************************************************************"
-    puts "#{x[pick-1].f_name} #{x[pick-1].l_name.delete ","}'s party is #{mem_pick}"
-    puts "#{x[pick-1].f_name} served congress's #{x[pick-1].served}"
-    puts "*****************************************************************"
-    next?
-    else
+      x = @g.collect{|m|m}
+      puts "*****************************************************************"
+      puts "#{x[pick-1].affl} #{x[pick-1].f_name} #{x[pick-1].l_name.delete ","}"
+      puts "#{x[pick-1].spec1a} #{x[pick-1].spec1b}"
+      puts "#{x[pick-1].spec3a} #{x[pick-1].spec3b.split("                    ").join("     ")}"
+      puts "#{x[pick-1].spec4a} #{x[pick-1].spec4b}"
+      puts "#{x[pick-1].spec2a} #{x[pick-1].spec2b}"
+      puts "*****************************************************************"
+      next?
+    elsif pick == 0
       search_query
+    else
+      goodbye
     end
-    #mem_pick
-       #binding.pry
   end
 
   def next?
-    puts "Would you like to search again? Yes or No"
+    puts "Would you like to search again? Yes or No?"
     ans = gets.chomp.downcase
     case ans
-
       when "yes"
         search_query
-        #list_members
         more
       when "no"
         goodbye
+      else
+        puts "I do not understand. Please try again. Yes or No?"
+        next?
     end
   end
 
   def goodbye
     puts "Thank you! Have a great day!"
-    abort
+    exit
   end
 end
