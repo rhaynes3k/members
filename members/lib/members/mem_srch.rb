b40049b7db9ec4b7d4ab870c8db90a25bc912aea
rhaynes3k <rhaynes3k@gmail.com>
2020-01-21 11:22:23 -0600

    reconfigured scrape method to return clean data, resolved details glitch and empty spaces in service history

--- members/lib/members/mem_srch.rb
+++ members/lib/members/mem_srch.rb
@@ -1,22 +1,27 @@
 class Mem_srch
 
-
   def self.loc_srch
-
     page = 1
     while page < 4
     rep = Nokogiri::HTML(open("https://www.congress.gov/members?q={%22congress%22:%22116%22}&searchResultViewType=expanded&KWICView=false&pageSize=250&page=#{page}"))
     congs = rep.css(".expanded").each do|cong|
         mem_info = cong.css(".result-heading a").text.split
-        f_name = mem_info[2]#rep.css(".expanded")[0].css(".result-heading a").text
+        f_name = mem_info[2, 3].join(" ")
         l_name = mem_info[1]
-        party = cong.css(".result-item span")[2].text
-        state = cong.css(".result-item span")[0].text
         affl = mem_info[0]
-        served = cong.css(".result-item li").text
-        #binding.pry
-        Member_details.new(f_name, l_name, party, state, affl, served)
-        #binding.pry
+        specs = cong.text.split("\n").map{|e|e.strip}.reject{|e|e.empty?}
+        spec1a = specs[3]
+        spec1b = specs[4]
+        spec2a = specs[5]
+        spec2b = specs[6]
+        spec3a = specs[7]
+        spec3b = specs[8]
+        spec4a = specs[9]
+        spec4b = specs[10]
+        spec5a= specs[5]
+        spec5b = specs[6]
+        Member_details.new(f_name, l_name, affl, spec1a, spec1b, spec2a, spec2b, spec3a, spec3b, spec4a, spec4b, spec5a, spec5b)
+#binding.pry
       end
       page += 1
     end
