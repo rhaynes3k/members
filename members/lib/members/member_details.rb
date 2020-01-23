55de61043f2fd172e08291e00ffba76ae7e366d0
rhaynes3k <rhaynes3k@gmail.com>
2020-01-22 20:47:46 -0600

    reconfigured attr accessors and inst. variables

--- members/lib/members/member_details.rb
+++ members/lib/members/member_details.rb
@@ -1,24 +1,17 @@
 class Member_details
 
-  attr_accessor :f_name, :l_name, :affl, :spec1a, :spec1b, :spec2a, :spec2b, :spec3a, :spec3b, :spec4a, :spec4b, :spec5a, :spec5b
-
+  attr_accessor :f_name, :l_name, :affl, :state, :district, :party, :served
   @@all = []
 
-  def initialize(f_name, l_name, affl, spec1a, spec1b, spec2a = nil, spec2b = nil, spec3a = nil, spec3b = nil, spec4a = nil, spec4b = nil, spec5a = nil, spec5b = nil)
+  def initialize(attributes)
+  attributes.each {|key, value| self.send(("#{key}="), value)}
     @f_name = f_name
     @l_name = l_name
     @affl = affl
-    @spec1a = spec1a
-    @spec2a = spec2a
-    @spec3a = spec3a
-    @spec4a = spec4a
-    @spec5a = spec5a
-    @spec1b = spec1b
-    @spec2b = spec2b
-    @spec3b = spec3b
-    @spec4b = spec4b
-    @spec5b = spec5b
-    # @pick = pick
+    @State = state
+    @District = district
+    @Party = party
+    @Served = served
 
     @@all << self
   end
@@ -27,16 +20,8 @@ class Member_details
     @@all
   end
 
-  # def states
-  #   self.all.collect do |s|
-  #     puts s.state
-  #     #binding.pry
-  #   end
-  # end
+  def assign_values
 
-  # def mem_more(pick)
-  #   self.all.collect do|m|
-  #     m.party = pick
-  #   end
-  # end
+  end
+
 end
